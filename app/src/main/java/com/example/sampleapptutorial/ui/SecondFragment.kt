049da14b6eb7111b0d98653bdf2d36d3b5d291ba
Shakhriyor Gafurov <smoke7772@gmail.com>
2021-07-14 14:52:17 +0500

    Few fixes here and there

--- app/src/main/java/com/example/sampleapptutorial/ui/SecondFragment.kt
+++ app/src/main/java/com/example/sampleapptutorial/ui/SecondFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sampleapptutorial.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -37,7 +38,7 @@ class SecondFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
-                TODO("Not yet implemented")
+                Log.e("error", "onFailure: ${t.localizedMessage}")
             }
         })
 
